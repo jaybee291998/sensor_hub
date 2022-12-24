116d79e558b3a2494eb53bc6a5805da776dcc47b
jayvee-pearlpay <jaybee291998@gmail.com>
2022-12-24 19:16:42 +0800

    Update views.py

--- channel/views.py
+++ channel/views.py
@@ -101,8 +101,8 @@ class ChannelEntryListAPIView(APIView):
                 number_of_hours = 1
             interval = timedelta(hours=number_of_hours)
             end_date = datetime.today() + timedelta(hours=1)
-            print(end_date)
             start_date = end_date - interval
+            print(start_date)
             channel_entries = channel.channel_entries.filter(timestamp__range=[start_date, end_date])
             field_count = channel.fields.all().count()
             included_fields = ['timestamp']
